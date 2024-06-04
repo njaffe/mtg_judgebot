179d3ee36baac34c5ae31e883d6cae3ea384b55c
Noah Jaffe <noah.h.jaffe@gmail.com>
2024-06-04 21:27:58 +0200

    trying different configurations and versions of langchain

--- config.py
+++ config.py
@@ -1,13 +1,11 @@
 import os
 
 API_KEY = os.environ["OPENAI_API_KEY"]
-DATA_PATH = "data"
-# DATA_PATH = "data/markdowns"
+DATA_PATH = "data" # adds mtg_rules.txt to db
 JSON_DIRECTORY = "data/jsons"
 CHROMA_PATH = "chroma"
 PDF_DIRECTORY='data/pdfs'
 MARKDOWN_DIRECTORY='data/markdowns'
-CHROMA_PATH = "chroma"
 PROMPT_TEMPLATE = """
         Answer the question based on the following context:
 
--- main.py
+++ main.py
@@ -4,6 +4,7 @@ import sys
 from utils.convert_to_md import process_inputs_in_directory
 from utils.create_database import create_database
 from utils.query_database import query_database
+from utils.query_reddit import query_reddit
 
 sys.path.append(os.path.abspath(os.path.join(os.path.dirname(__file__), os.path.pardir)))
 from config import API_KEY, DATA_PATH, CHROMA_PATH
@@ -13,6 +14,9 @@ def main():
     process_inputs_in_directory()
     create_database()
     query_database()
+    query_reddit()
+
+    # what if reddit and database disagree? 
 
 if __name__ == "__main__":
     main()
